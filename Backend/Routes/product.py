48dd0e54af463be31f80a85ed7d7e6c23f5edcb6
cRONOS <softcronos@gmail.com>
2023-12-19 19:46:49 -0300

    Update product.py
    elif de metodo PUT, para no hacer tantas rutas

--- Backend/Routes/product.py
+++ Backend/Routes/product.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request, session
 #token y permisos
-from auth import token_required
+from auth import token_required, user_resources
 
 #Clases
 from Models.product import Product
@@ -8,8 +8,9 @@ from BD.database import ConnBD as bd
 
 productHandle = Blueprint('product', __name__)
 
-@productHandle.route('/', methods = ['GET', 'POST'])
+@productHandle.route('/', methods = ['GET', 'POST', 'PUT'])
 @token_required
+#@user_resources
 def usersData():
     if request.method == 'GET':
         try:
@@ -17,6 +18,15 @@ def usersData():
             usersList = Product.getAllProduct(id_company)
             return usersList
         except Exception as e:
-            return jsonify({"error de ruta": str(e)}), 500
+            return jsonify({"API error": str(e)}), 500
     elif request.method == 'POST':
         pass
+    elif request.method == 'PUT':
+        if 'id-product' in request.headers:
+            print("es  un put con id")
+            id_product = request.headers['id-product']
+            return jsonify({"aca deberia actualizar el id:": str(id_product)}), 200
+        return jsonify({"API error:": "no id_product found"}), 401
+    
+    
+umHandle = Blueprint('um', __name__)

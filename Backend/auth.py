acbb2b14c56a47ff6a07454338b3172a95c4dfa4
cRONOS <softcronos@gmail.com>
2023-12-19 19:46:14 -0300

    Update auth.py
    agregado de user_resoirces, no terminado

--- Backend/auth.py
+++ Backend/auth.py
@@ -6,7 +6,8 @@ from functools import wraps
 from config import appCfg
 #configuracion de conect BD
 from BD.database import ConnBD as bd
-
+#users class for sql
+from Models.user import getUserPermission
 
 #validacion de token
 def token_required(func):
@@ -70,6 +71,25 @@ def client_resource(func):
     return decorated
 
 #validacion recurso usuario
+def user_resourcesORIGIN(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        # Imprime los argumentos pasados al decorador
+        print("Argumentos en user_resources: ", kwargs)
+        # Extrae el ID de usuario desde la ruta (path) de la solicitud
+        id_user_route = kwargs['id_user']
+        # Obtiene el ID de usuario desde las cabeceras de la solicitud
+        user_id = request.headers['user-id']
+        # Compara los IDs para asegurarse de que el usuario tiene acceso al recurso del usuario
+        if int(id_user_route) != int(user_id):
+            # Si no tiene acceso, devuelve un mensaje de error y un código de estado 401 (No autorizado)
+            return jsonify({"message": "No tiene permisos para acceder a este recurso"}), 401
+        # Si todo está en orden, ejecuta la función original
+        return func(*args, **kwargs)
+    # Devuelve la función decorada
+    return decorated
+
+#validacion recurso usuario
 def user_resources(func):
     @wraps(func)
     def decorated(*args, **kwargs):
@@ -83,6 +103,10 @@ def user_resources(func):
         if int(id_user_route) != int(user_id):
             # Si no tiene acceso, devuelve un mensaje de error y un código de estado 401 (No autorizado)
             return jsonify({"message": "No tiene permisos para acceder a este recurso"}), 401
+        #Ahora chequea q tenga permisos para la operacion
+        if request.method == 'PUT':
+            user_permissions = getUserPermission(user_id)
+            print(user_permissions) #aca deveria consultar si tiene permisos de u (Update)
         # Si todo está en orden, ejecuta la función original
         return func(*args, **kwargs)
     # Devuelve la función decorada
